a0732ac4036b240c15a46b530b29c22b0c27fecd
Julio Andres <julio.olivares.a@gmail.com>
2024-01-05 07:47:37 -0300

    actualizando openai api a >= 1.0.0

--- completions_endpoint/app.py
+++ completions_endpoint/app.py
@@ -1,11 +1,11 @@
-import json
 import openai
+
 
 def complete(text):
     openai.api_key = "TU_API_KEY"
-    completion = openai.Completion.create(
+    completion = openai.completions.create(
         model="gpt-3.5-turbo-instruct",
-        prompt='''
+        prompt=f"""
 Eres un chatbot asistente virtual, que ayuda a los usuarios a aprender inglés.
 Assistant: Hola soy tu asistente, preguntame lo que quieras.
 User: Hola, como se dice hola en inglés?
@@ -13,15 +13,16 @@ Assistant: se dice 'Hi'
 User: {text}
 Assistant: aqui
 User: 
-'''.format(text=text),
+""",
         max_tokens=200,
         temperature=0.5,
         frequency_penalty=1,
-        user="julio"
+        user="julio",
     )
 
     return completion
 
+
 response = complete("Cómo se dice 'Donde esta el baño?'")
 
-print(json.dumps(response, indent=4))
+print(response.model_dump_json())
